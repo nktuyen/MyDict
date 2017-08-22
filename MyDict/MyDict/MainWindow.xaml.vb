c398ab2c1881572303c8af405eb184ad5fe7936f
nktuyen@hotmail.com <nktuyen@hotmail.com>
2017-08-22 22:41:37 +0700

    Add source code

---
+++ MyDict/MyDict/MainWindow.xaml.vb
@@ -0,0 +1,81 @@
+﻿Class MainWindow
+    Private m_bPlaceHolderKeyword As Boolean = False
+
+    Private Function ViewVocabulary(ByVal strVocabulary As String) As Boolean
+        Return False
+    End Function
+
+    Private Sub GridSplitter_DragCompleted(sender As Object, e As Primitives.DragCompletedEventArgs)
+    End Sub
+
+    Private Sub btnNext_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles btnNext.MouseLeftButtonUp
+
+    End Sub
+
+    Private Sub btnPrev_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles btnPrev.MouseLeftButtonUp
+
+    End Sub
+
+    Private Sub cboFilter_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles cboFilter.SelectionChanged
+        Dim strVocabulary As String = ""
+        If (lstWords.SelectedItem Is Nothing) Then
+            Return
+        End If
+        strVocabulary = CStr(lstWords.SelectedItem)
+        m_bPlaceHolderKeyword = False
+        txtKeyword.Text = strVocabulary
+        Call ViewVocabulary(strVocabulary)
+    End Sub
+
+    Private Sub txtKeyword_TextChanged(sender As Object, e As TextChangedEventArgs) Handles txtKeyword.TextChanged
+
+    End Sub
+
+    Private Sub btnAdd_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles btnAdd.MouseLeftButtonUp
+
+    End Sub
+
+    Private Sub btnDelete_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles btnDelete.MouseLeftButtonUp
+
+    End Sub
+
+    Private Sub btnPrint_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles btnPrint.MouseLeftButtonUp
+
+    End Sub
+
+    Private Sub btnEdit_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles btnEdit.MouseLeftButtonUp
+
+    End Sub
+
+    Private Sub btnSettings_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles btnSettings.MouseLeftButtonUp
+
+    End Sub
+
+    Private Sub wndMain_Loaded(sender As Object, e As RoutedEventArgs) Handles wndMain.Loaded
+        If (Not IsDecryptedDictionary("EV.accdb")) Then
+            Call DecryptDictionary("EV.accdb")
+        End If
+    End Sub
+
+    Private Sub wndMain_Unloaded(sender As Object, e As RoutedEventArgs) Handles wndMain.Unloaded
+        If (IsDecryptedDictionary("EV.accdb")) Then
+            Call EncryptDictionary("EV.accdb")
+        End If
+    End Sub
+
+    Private Sub txtKeyword_GotFocus(sender As Object, e As RoutedEventArgs) Handles txtKeyword.GotFocus
+        If (m_bPlaceHolderKeyword) Then
+            m_bPlaceHolderKeyword = False
+            txtKeyword.Text = ""
+        Else
+            txtKeyword.SelectAll()
+        End If
+    End Sub
+
+    Private Sub txtKeyword_LostFocus(sender As Object, e As RoutedEventArgs) Handles txtKeyword.LostFocus
+        If (txtKeyword.Text.Length <= 0) Then
+            txtKeyword.Text = "Nhập vào từ khóa..."
+            m_bPlaceHolderKeyword = True
+        End If
+    End Sub
+End Class
---
+++ MyDict/MyDict/Utils.vb
@@ -0,0 +1,80 @@
+﻿Imports System.IO
+
+
+Module Utils
+    Private stdHeader() As Byte = {0, 1, 0, 0, 83, 116, 97, 110, 100, 97, 114, 100, 32, 65, 67, 69, 32, 68, 66, 0, 2, 0, 0, 0, 181, 110, 3, 98, 96, 9, 194, 85}
+
+    Public Function IsDecryptedDictionary(ByVal strPath As String) As Boolean
+        If (strPath Is Nothing) Then
+            Return False
+        End If
+        '
+        If (strPath.Length <= 0) Then
+            Return False
+        End If
+        '
+        Dim dicFile As FileStream = Nothing
+        Try
+            dicFile = New FileStream(strPath, FileMode.Open, FileAccess.Read)
+        Catch
+            dicFile = Nothing
+        End Try
+        If (dicFile Is Nothing) Then
+            Return False
+        End If
+        '
+        Dim reader As BinaryReader
+        Try
+            reader = New BinaryReader(dicFile)
+        Catch ex As Exception
+            reader = Nothing
+        End Try
+        '
+        If (reader Is Nothing) Then
+            Return False
+        End If
+        '
+        Dim dicHeader() As Byte = reader.ReadBytes(stdHeader.Length)
+        If (dicHeader.Length <= 0) Then
+            Return False
+        End If
+        '
+        If (dicHeader.Length <> stdHeader.Length) Then
+            Return False
+        End If
+        '
+        For i As Integer = 0 To stdHeader.Length - 1
+            If (dicHeader(i) <> stdHeader(i)) Then
+                Return False
+            End If
+        Next i
+        '
+        reader.Close()
+        dicFile.Close()
+        Return True
+    End Function
+
+    Public Function EncryptDictionary(ByVal strPath As String) As Boolean
+        If (strPath Is Nothing) Then
+            Return False
+        End If
+        '
+        If (strPath.Length <= 0) Then
+            Return False
+        End If
+        '
+
+    End Function
+
+    Public Function DecryptDictionary(ByVal strPath As String) As Boolean
+        If (strPath Is Nothing) Then
+            Return False
+        End If
+        '
+        If (strPath.Length <= 0) Then
+            Return False
+        End If
+        '
+
+    End Function
+End Module
