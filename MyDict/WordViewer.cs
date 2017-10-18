23177a45e265163e58cda55af9df2a04b49b1221
nktuyen@hotmail.com <nktuyen@outlook.com>
2017-10-19 02:07:08 +0700

    Create source code

---
+++ MyDict/WordViewer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyDict
+{
+    public partial class WordViewer : UserControl
+    {
+        public event EventHandler OnRefreshClicked;
+        public event EventHandler OnLoveClicked;
+        public event EventHandler OnPrintClicked;
+        public event EventHandler OnEditClicked;
+        public event EventHandler OnPreferencesClicked;
+
+        public WordViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            if (null != OnRefreshClicked)
+            {
+                this.OnRefreshClicked(sender, e);
+            }
+        }
+
+        private void btnLove_Click(object sender, EventArgs e)
+        {
+            if(null != OnLoveClicked)
+            {
+                this.OnLoveClicked(sender, e);
+            }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            if (null != OnPrintClicked)
+            {
+                this.OnPrintClicked(sender, e);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (null != OnEditClicked)
+            {
+                this.OnEditClicked(sender, e);
+            }
+        }
+
+        private void btnPreferences_Click(object sender, EventArgs e)
+        {
+            if (null != OnPreferencesClicked)
+            {
+                this.OnPreferencesClicked(sender, e);
+            }
+        }
+    }
+}
---
+++ MyDict/frmMain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyDict
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void cbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtKeyword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbVocabularies_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            if (null != assembly)
+            {
+                this.Text = assembly.GetName().Name + " " + assembly.GetName().Version.Major + "." + assembly.GetName().Version.Minor + "." + assembly.GetName().Version.Revision + "." + assembly.GetName().Version.Build;
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void wrdViewer_OnEditClicked(object sender, EventArgs e)
+        {
+            MessageBox.Show("Edit");
+        }
+
+        private void wrdViewer_OnLoveClicked(object sender, EventArgs e)
+        {
+            MessageBox.Show("Love");
+        }
+
+        private void wrdViewer_OnPreferencesClicked(object sender, EventArgs e)
+        {
+            MessageBox.Show("Preferences");
+        }
+
+        private void wrdViewer_OnPrintClicked(object sender, EventArgs e)
+        {
+            MessageBox.Show("Print");
+        }
+
+        private void wrdViewer_OnRefreshClicked(object sender, EventArgs e)
+        {
+            MessageBox.Show("Refresh");
+        }
+    }
+}
