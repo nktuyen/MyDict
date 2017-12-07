5b96eca77d8c27bb83ad7b2f1c9aed0fbf6a6d4c
Tuyen <nktuyen@hotmail.com>
2017-12-07 12:37:46 +0700

    Re-structure

---
+++ Source/MyDict/WordViewer.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace MyDict
+{
+    partial class WordViewer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnLove = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnPreferences = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.Location = new System.Drawing.Point(108, 1);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(60, 25);
+            this.btnRefresh.TabIndex = 0;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnLove
+            // 
+            this.btnLove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLove.Location = new System.Drawing.Point(170, 1);
+            this.btnLove.Name = "btnLove";
+            this.btnLove.Size = new System.Drawing.Size(40, 25);
+            this.btnLove.TabIndex = 1;
+            this.btnLove.Text = "Love";
+            this.btnLove.UseVisualStyleBackColor = true;
+            this.btnLove.Click += new System.EventHandler(this.btnLove_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPrint.Location = new System.Drawing.Point(212, 1);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(40, 25);
+            this.btnPrint.TabIndex = 2;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnPreferences
+            // 
+            this.btnPreferences.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPreferences.Location = new System.Drawing.Point(338, 1);
+            this.btnPreferences.Name = "btnPreferences";
+            this.btnPreferences.Size = new System.Drawing.Size(74, 26);
+            this.btnPreferences.TabIndex = 3;
+            this.btnPreferences.Text = "Preferences";
+            this.btnPreferences.UseVisualStyleBackColor = true;
+            this.btnPreferences.Click += new System.EventHandler(this.btnPreferences_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEdit.Location = new System.Drawing.Point(254, 1);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(40, 25);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddNew.Location = new System.Drawing.Point(296, 1);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(40, 25);
+            this.btnAddNew.TabIndex = 5;
+            this.btnAddNew.Text = "New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // WordViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnPreferences);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnLove);
+            this.Controls.Add(this.btnRefresh);
+            this.Name = "WordViewer";
+            this.Size = new System.Drawing.Size(416, 266);
+            this.VisibleChanged += new System.EventHandler(this.WordViewer_VisibleChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnLove;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnPreferences;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAddNew;
+    }
+}
---
+++ Source/MyDict/WordViewer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyDict
+{
+    public partial class WordViewer : UserControl
+    {
+        public event EventHandler OnRefresh;
+        public event EventHandler OnLove;
+        public event EventHandler OnPrinting;
+        public event EventHandler OnEdit;
+        public event EventHandler OnPreferences;
+        public event EventHandler OnAddNew;
+
+        public WordViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            if (null != OnRefresh)
+            {
+                this.OnRefresh(sender, e);
+            }
+        }
+
+        private void btnLove_Click(object sender, EventArgs e)
+        {
+            if(null != OnLove)
+            {
+                this.OnLove(sender, e);
+            }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            if (null != OnPrinting)
+            {
+                this.OnPrinting(sender, e);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (null != OnEdit)
+            {
+                this.OnEdit(sender, e);
+            }
+        }
+
+        private void btnPreferences_Click(object sender, EventArgs e)
+        {
+            if (null != OnPreferences)
+            {
+                this.OnPreferences(sender, e);
+            }
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            if (null != OnAddNew)
+            {
+                this.OnAddNew(sender, e);
+            }
+        }
+
+        private void WordViewer_VisibleChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
