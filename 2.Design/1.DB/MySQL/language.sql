bdecfae81b5625456f323024542f46b92d6fa35f
nktuyen <nktuyen@hotmail.com>
2018-06-03 02:25:46 +0700

    add field to language table DB

--- 2.Design/1.DB/MySQL/language.sql
+++ 2.Design/1.DB/MySQL/language.sql
@@ -1,9 +1,11 @@
 CREATE TABLE tbl_language(
-	id SMALLINT(6) UNSIGNED NOT NULL COMMENT 'Mã ngôn ngữ',
+	id SMALLINT(6) UNSIGNED NOT NULL COMMENT 'Mã ngôn ngữ (number)',
+	code VARCHAR(50)  NOT NULL COMMENT 'Mã ngôn ngữ',
 	name VARCHAR(50)  NOT NULL COMMENT 'Tên ngôn ngữ',
 	title VARCHAR(50)  NOT NULL COMMENT 'Tiêu đề'
 ) DEFAULT CHARSET=utf8 COMMENT='Ngôn ngữ';
 ALTER TABLE tbl_language
 	ADD PRIMARY KEY (id),
 	ADD UNIQUE KEY UN_tbl_language2 (id),
-	ADD UNIQUE KEY UN_tbl_language3 (name);
+	ADD UNIQUE KEY UN_tbl_language3 (code),
+	ADD UNIQUE KEY UN_tbl_language4 (name);
