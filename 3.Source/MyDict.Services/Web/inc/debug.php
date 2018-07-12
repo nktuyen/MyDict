9977590c9488d7693e112a03adc7039418fc4230
nktuyen <nktuyen@hotmail.com>
2018-07-13 01:35:50 +0700

    Modify DB design file structure

---
+++ 3.Source/MyDict.Services/Web/inc/debug.php
@@ -0,0 +1,23 @@
+<?php
+    defined('__SIGNAL__') or die('Access restricted.');
+?>
+
+<?php
+    class Debug
+    {
+        protected function __construct() {
+
+        }
+        private function __clone() {
+
+        }
+
+        private function __wakeup() {
+
+        }
+
+        public static function print($string) {
+
+        }
+    }
+?>
